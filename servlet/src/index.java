614599ce7e4970ea8fc137f60a4cc0b0653801a8
papo9924 <papogabrriel@gmail.com>
2014-06-13 22:40:48 -0500

    JSP

--- servlet/src/index.java
+++ servlet/src/index.java
@@ -1,9 +1,10 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,127 +13,135 @@ import javax.servlet.http.HttpServletResponse;
 
 public class index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	protected void doGet(HttpServletRequest request, 
-			HttpServletResponse response) throws ServletException, IOException {
-			response.setContentType("text/html");
-			PrintWriter writer = response.getWriter();
-			writer.print("<html>"
-					+ "<head>"
-					+ "<title>Add task</title>"
-					+ "</head>"
-					+ "<body>"
-					+ "<form method=\"POST\">"
-					+ "<table>"
 
-					+ "<tr>"
-					+ "<td>Title:</td>"
-					+ "<td>"
-					+ "<input type=\"text\"name=\"title\"/>"
-					+ "</td>"
-					+ "</tr>"
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter writer= response.getWriter();
+		writer.print("<html>"
+				+ "<head>"
+				+"<title>Add Task</title>"
+				+ "</head>"
+				+"<body>"
+				+"<form method=\"POST\">"
+				+"<table>"
+				+"<tr>"
+				+"<td>Tittle:</td>"
+				+"<td>"
+				+"<input type=\"text\" name=\"title\"/>"
+				+"</td>"
+				+"</tr>"
 
-					+ "<tr>"
-					+ "<td>Description:</td>"
-					+ "<td>"
-					+ "<input type=\"text\"name=\"description\"/>"
-					+ "</td>"
-					+ "</tr>"
+				+"<tr>"
+				+"<td>Description:</td>"
+				+"<td>"
+				+"<input type=\"text\" name=\"description\"/>"
+				+"</td>"
+				+"</tr>"
 
-					+ "<tr>"
-					+ "<td>State:</td>"
-					+ "<td>"
-					+"<select name=\"state\">"
-					+ "<option value=\"BACKLOG\">Backlog</option>"
-					+ "<option value=\"TO_DO\">To do</option>"
-					+ "<option value=\"IN_PROGRESS\">In progress</option>"
-					+ "<option value=\"DONE\">Done</option>"
-					+ "</select>"
-					+ "</td>"
-					+ "</tr>"
+				+"<tr>"
+				+"<td>Category:</td>"
+				+"<td>"
+				+"<input type=\"text\" name=\"category\"/>"
+				+"</td>"
+				+"</tr>"
 
-					+ "<tr>"
-					+ "<td>Category:</td>"
-					+ "<td>"
-					+ "<input type=\"text\"name=\"category\"/>"
-					+ "</td>"
-					+ "</tr>"
 
-					+ "<tr>"
-					+ "<td>Priority:</td>"
-					+ "<td>"
-					+ "<input type=\"text\"name=\"priority\"/>"
-					+ "</td>"
-					+ "</tr>"
+				+"<tr>"
+				+"<td>Owner:</td>"
+				+"<td>"
+				+"<input type=\"text\" name=\"owner\"/>"
+				+"</td>"
+				+"</tr>"
 
-					+ "<tr>"
-					+ "<td>Owner:</td>"
-					+ "<td>"
-					+ "<input type=\"text\"name=\"owner\"/>"
-					+ "</td>"
-					+ "</tr>"
+				+"<tr>"
+				+"<td>Prioridad:</td>"
+				+"<td>"
+				+"<input type=\"text\" name=\"priority\"/>"
+				+"</td>"
+				+"</tr>"
 
-					+ "<tr>"
-					+ "<td>Due date:</td>"
-					+ "<td>"
-					//+ "<input type=\"text\"name=\"dueDate\"/>"
-					+"<input type=\"text\" name=\"due_date\" value=\""
-					+ new Date() + "\"/>"
-					+ "</td>"
-					+ "</tr>"
+				+"<tr>"
+				+"<td>State:</td>"
+				+"<td>"
+				+"<select name=\"state\">"
+				+"<option value=\"BACKLOG\"selected>BackloG</option>"
+				+"<option value=\"TO_DO\">To do</option>"
+				+"<option value=\"IN_PROGRESS\">In progress</option>"
+				+"<option value=\"DONE\">done</option>"
+				+"</select>"	
+				+"</td>"
+				+"</tr>"
 
-					+ "<tr>"
-					+ "<td>Add Task:</td>"
-					+ "<td>"
-					+ "<input type=\"submit\"value=\"Add Task\"/>"
-					+ "</td>"
-					+ "</tr>"
+				+"<tr>"
+				+"<td>Due Date:</td>"
+				+"<td>"
+				+"<input type=\"text\" name=\"duedate\"/>" 
+				+"</td>"
+				+"</tr>"
 
-					+ "</table>"
-					+ "</form>"
-					+ "</body>"
-					+ "</html>"
-					);
-	}
+				+"<tr>"
+				+"<td colspan=\"2\">"
+				+"<input type=\"submit\" value=\"Add\"/>"
+				+"</td>"
+				+"<tr>"
 
-	protected void doPost(HttpServletRequest request, 
-			HttpServletResponse response) throws ServletException, IOException {
-				Task task = new Task();
 
-		task.setTitle(request.getParameter("title"));
-		task.setDescription(request.getParameter("description"));
-		task.setOwner(request.getParameter("owner"));
-		task.setCategory(new Category(request.getParameter("category")));
+				+"</table>"
+				+"</form>"
+				+"</body>"
+				+"</html>");
+	} 
 
-		SimpleDateFormat formatter = new SimpleDateFormat(
-				"EEE MMM d HH:mm:ss zzz yyyy");
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {		
+
+		Task t=new Task();
+
+		t.setTitle(request.getParameter("title"));
+		t.setDescription(request.getParameter("description"));
+		t.setOwner(request.getParameter("owner"));
+		t.setCategory(new Category(request.getParameter("category")));		
+
+		SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM d HH:mm:ss zzz yyyy");
+
 		try {
-			task.setDueDate(formatter.parse(request.getParameter("due_date")));
+			t.setDueDate(formatter.parse(request.getParameter("duedate")));
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		try {
-			task.setState(State.valueOf(request.getParameter("state")));
+			t.setState(State.valueOf(request.getParameter("state")));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		try {
-			task.setPriority(Short.parseShort(request.getParameter("priority")));
+			t.setPriority(Short.parseShort(request.getParameter("priority")));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		task.setCreateDate(new Date());
-
-		Program.dashboard.add(task);
+		Program.dashboard.add(t);
 
 		response.setContentType("text/html");
-		PrintWriter writer2 = response.getWriter();
-		writer2.print("<html><body>" + task.toHtml() + "</body></html>");
+		PrintWriter writer= response.getWriter();
+
+		writer.print("<html>"
+		+"<body>"
+		+"Title: " +request.getParameter("title")
+		+"<br>"
+		+"Description: "+request.getParameter("description")
+		+"<br>"
+		+"Owner: "+request.getParameter("owner")
+		+"<br>"
+		+"Category: "+request.getParameter("category")
+		+"<br>"
+		+"Due Date: "+request.getParameter("duedate")
+		+"<br>"
+		+"Priority: "+request.getParameter("priority")
+		+"<br>"
+		+"State: "+request.getParameter("state")
+		+ "</table>"
+		+"</body>"
+		+"</html>");
 	}
-
-
-
-
 
 }
